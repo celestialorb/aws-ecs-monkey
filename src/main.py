85072d0cd108cc57c395ca2856a156e9b026ba36
Christopher Busby <chris@controlant.com>
2022-02-10 15:44:11 +0000

    adding help messages and choices for action type

--- src/main.py
+++ src/main.py
@@ -9,9 +9,13 @@ import tasks
 # Read in the configuration.
 p = configargparse.get_argument_parser()
 p.add("-c", "--config-file", required=False, is_config_file=True)
-p.add("--cluster-name", env_var="AWS_ECS_MONKEY_CLUSTER_NAME")
-p.add("--action-type", env_var="AWS_ECS_MONKEY_ACTION_TYPE")
-p.add("--action-dry-run", env_var="AWS_ECS_MONKEY_ACTION_DRY_RUN", action="store_true")
+p.add("--cluster-name", env_var="AWS_ECS_MONKEY_CLUSTER_NAME",
+      help="The name of the AWS ECS cluster to inject chaos into.")
+p.add("--action-type", env_var="AWS_ECS_MONKEY_ACTION_TYPE",
+      choices=["nodes", "services", "tasks"],
+      help="The type of chaos action to inject, only one can be specified.")
+p.add("--action-dry-run", env_var="AWS_ECS_MONKEY_ACTION_DRY_RUN", action="store_true",
+      help="If provided, no actions will be taken against the cluster.")
 
 config = p.parse_args()
 logger.info(config)
